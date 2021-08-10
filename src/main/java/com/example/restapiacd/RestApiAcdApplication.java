fc9f1456d834296f91456cfd4994bbca00fedf40
Anıl Can Dikmedaş <dikmedasanilcan@gmail.com>
2021-08-10 20:24:19 +0300

    make it better

---
+++ src/main/java/com/example/restapiacd/RestApiAcdApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiacd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiAcdApplication.class, args);
+    }
+
+}
